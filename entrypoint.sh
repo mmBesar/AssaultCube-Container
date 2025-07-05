8f888192f33696d9195495ca9fe76445b40249d3
Mohammed Besar <35855690+mmBesar@users.noreply.github.com>
2025-07-06 01:37:15 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,20 +3,15 @@
 
 ARGS=""
 
-# Server name (description)
 [ -n "$SERVER_NAME" ]   && ARGS="$ARGS -n '$SERVER_NAME'"
-# Max clients
 [ -n "$MAX_CLIENTS" ]   && ARGS="$ARGS -c $MAX_CLIENTS"
-# Map rotation file
 [ -n "$MAP_ROTATION" ]  && ARGS="$ARGS -r $MAP_ROTATION"
-# Masterserver (e.g. 'localhost')
 [ -n "$MASTER_SERVER" ] && ARGS="$ARGS -m $MASTER_SERVER"
-# Auth key for public registration
 [ -n "$AUTH_KEY" ]      && ARGS="$ARGS -Y $AUTH_KEY"
-# Extra options
 [ -n "$EXTRA_OPTS" ]    && ARGS="$ARGS $EXTRA_OPTS"
 
-# Include servercmdline.txt if present
+# Include custom servercmdline if mounted
 [ -f config/servercmdline.txt ] && ARGS="$ARGS -C config/servercmdline.txt"
 
-exec ./bin_unix/native_server $ARGS
+# Launch AssaultCube via its provided script
+exec ./server.sh $ARGS
